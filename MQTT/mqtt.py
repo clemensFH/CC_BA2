9bcc70e22f642f3b3b4c45fefda092953f2647f7
clemensFH <clemens.hoerhan@stud.fh-campuswien.ac.at>
2025-05-08 10:32:01 +0200

    mqtt update

--- MQTT/mqtt.py
+++ MQTT/mqtt.py
@@ -9,11 +9,15 @@ print(data + " len: " + str(len(data)))
 
 byte_data = bytes.fromhex(data)
 
-p = mqtt.MQTT()/mqtt.MQTTConnect(clientIdlen=len(byte_data[:5]), clientId=byte_data[:5], protoname='MQTT', protolevel=5)
+p = mqtt.MQTT()/mqtt.MQTTConnect(clientId=byte_data[:5],
+                                 willflag=1, usernameflag=1, passwordflag=1,
+                                 willtopic=byte_data[:5], willmsg=byte_data[:5],
+                                 username=byte_data[:5], password=byte_data[0:5],
+                                 protoname='MQTT', cleansess=1, klive=60, protolevel=6)
 a = mqtt.MQTT(QOS=1)/mqtt.MQTTPublish(msgid=1, topic=byte_data[:5], value=byte_data[5:10])
 
 # TCP-Verbindung aufbauen
-ip = IP(dst="127.0.0.1")
+ip = IP(dst="10.0.0.12")
 sport = RandShort()
 tcp_syn = TCP(sport=sport, dport=1883, flags="S", seq=1000)
 synack = sr1(ip / tcp_syn, timeout=2)
@@ -24,6 +28,10 @@ if synack and synack.haslayer(TCP):
     ack = synack.seq + 1
     tcp_ack = TCP(sport=sport, dport=1883, flags="A", seq=seq, ack=ack)
     send(ip / tcp_ack)
+
+    tcp_push = TCP(sport=sport, dport=1883, flags="PA", seq=seq, ack=ack)
+    send(ip / tcp_push / p)
+    print(p.summary())
 
     # MQTT CONNECT senden
     tcp_push = TCP(sport=sport, dport=1883, flags="PA", seq=seq, ack=ack)
